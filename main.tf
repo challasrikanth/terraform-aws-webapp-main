d882e922e837170f7ce372ad24992bd0d5e98813
srikanth <csri@nuuday.com>
2026-03-03 14:08:54 +0530

    updated

--- main.tf
+++ main.tf
@@ -8,16 +8,15 @@ module "vpc" {
 
 
 module "alb" {
-  source         = ".git::https://github.com/challasrikanth/terraform-aws-webapp.git//modules/alb"
+  source         = "git::https://github.com/challasrikanth/terraform-aws-webapp.git//modules/alb"
   public_subnets = module.vpc.public_subnet_ids
   vpc_id         = module.vpc.vpc_id
 }
 
-/* module "ec2_asg" {
-  source           = "./modules/ec2-asg"
+module "ec2_asg" {
+  source           = "git::https://github.com/challasrikanth/terraform-aws-webapp.git//modules/asg"
   ami              = var.ami
   instance_type    = var.instance_type
   private_subnets  = module.vpc.private_subnet_ids
   target_group_arn = module.alb.target_group_arn
 }
- */
--- variables.tf
+++ variables.tf
@@ -5,3 +5,12 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
+variable "ami" {
+    default = "ami-0220d79f3f480ecf5"
+  
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+  
+}
