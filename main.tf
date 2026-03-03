98524b3a99c4b57cfb9006e9f242e262f736facb
srikanth <csri@nuuday.com>
2026-03-03 13:24:32 +0530

    alb files added

---
+++ main.tf
@@ -0,0 +1,23 @@
+
+module "vpc" {
+    source = "git::https://github.com/challasrikanth/terraform-aws-webapp.git//modules/vpc"
+    project = var.project
+    environment = var.environment
+
+}
+
+
+module "alb" {
+  source         = ".git::https://github.com/challasrikanth/terraform-aws-webapp.git//modules/alb"
+  public_subnets = module.vpc.public_subnet_ids
+  vpc_id         = module.vpc.vpc_id
+}
+
+/* module "ec2_asg" {
+  source           = "./modules/ec2-asg"
+  ami              = var.ami
+  instance_type    = var.instance_type
+  private_subnets  = module.vpc.private_subnet_ids
+  target_group_arn = module.alb.target_group_arn
+}
+ */
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "project" {
+    default = "webapp"
+}
+
+variable "environment" {
+    default = "dev"
+}
